8e60e301ad22f05d466f8d04775e63de06087bae
Damon Oehlman <damon.oehlman@gmail.com>
2017-05-29 16:58:16 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: [
+      "airbnb-base"
+    ],
+    plugins: [
+        "import"
+    ],
+    rules: {
+      "space-before-function-paren": [0],
+      "indent": ["error", 2]
+    }
+};
