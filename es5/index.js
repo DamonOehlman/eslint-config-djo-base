49fd1d14a39edac11fd0ad185735395f2a18e87d
Damon Oehlman <damon.oehlman@gmail.com>
2017-05-31 22:49:11 +1000

    adding es5 config

---
+++ es5/index.js
@@ -0,0 +1,11 @@
+module.exports = require('..');
+module.exports.rules = Object.extend({}, module.exports.rules, {
+  "no-var": [0],
+  "prefer-arrow-callback": [0],
+  "func-names": [0],
+  "prefer-rest-params": [0],
+  "no-plusplus": [0],
+  "object-shorthand": [0],
+  "prefer-spread": [0],
+  "prefer-template": [0]
+});
